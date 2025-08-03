334a9b7091d9703c3b0377367be199c1d0eff173
froshy <robinliu81@gmail.com>
2025-08-02 23:02:18 -0400

    add year validator and tests

--- portfolio_blog/portfolio/models.py
+++ portfolio_blog/portfolio/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator 
+import datetime
 
 # Create your models here.
+def current_year():
+    return datetime.date.today().year    
 
 class ProjectEntry(models.Model):
 
     proj_name = models.CharField(primary_key=True, max_length=50, verbose_name="Project Name")
     description = models.TextField(verbose_name="Description")
-    start_year = models.IntegerField(null=False, verbose_name="Development Start Year")
+    start_year = models.IntegerField(null=False, verbose_name="Development Start Year",
+                                     validators=[MinValueValidator(1900), MaxValueValidator(current_year)], help_text="Enter a year between 1900 and current year")
     git_repo = models.URLField(null=False, verbose_name="Github Repository")
 
--- portfolio_blog/portfolio/tests.py
+++ portfolio_blog/portfolio/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+from .models import ProjectEntry
 
 # Create your tests here.
+
+class ProjectEntryTests(TestCase):
+    project_name = "project name test"
+    description = "description test"
+    git_repo = "http://www.gitrepo.com"
+
+    def test_validator_start_year_valid(self):
+        entry = ProjectEntry(proj_name=self.project_name, description=self.description, start_year=2020, git_repo=self.git_repo) 
+        entry.full_clean()
+
+    def test_validator_start_year_early(self):
+        entry = ProjectEntry(proj_name=self.project_name, description=self.description, start_year=1899, git_repo=self.git_repo)
+        with self.assertRaises(ValidationError):
+            entry.full_clean()
+    def test_validator_start_year_future(self):
+        year = timezone.now().year + 1
+
+        entry = ProjectEntry(proj_name=self.project_name, description=self.description, start_year=year, git_repo=self.git_repo)
+        with self.assertRaises(ValidationError):
+            entry.full_clean()
