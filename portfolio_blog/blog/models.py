55b870393d8bdcf26c4002fbabe66481b24a8747
froshy <robinliu81@gmail.com>
2025-08-07 22:01:18 -0400

    blog save and slug fields

--- portfolio_blog/blog/models.py
+++ portfolio_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.text import slugify
 import datetime
 
 class BlogEntry(models.Model):
@@ -8,4 +9,13 @@ class BlogEntry(models.Model):
     blog_title = models.CharField(primary_key=True, max_length=50, verbose_name="Blog Title")
     date_time = models.DateTimeField(default=timezone.now, verbose_name="Date & Time")
     content = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    def get_absolute_url(self):
+        return reverse('detail', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.blog_title)
+        super().save(*args, **kwargs)
 
--- portfolio_blog/blog/urls.py
+++ portfolio_blog/blog/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path, include
 
-# from .views import Index, Detail
+from .views import index
 urlpatterns = [
+        path("", index, name="index")
         ]
