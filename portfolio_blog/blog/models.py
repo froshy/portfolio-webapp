dd53818f2285bc318ba1f9d96fe80f742873b06a
froshy <robinliu81@gmail.com>
2025-08-14 22:58:19 -0400

    default foreign field

--- portfolio_blog/blog/models.py
+++ portfolio_blog/blog/models.py
@@ -3,8 +3,17 @@ from django.urls import reverse
 from django.utils import timezone
 from django.utils.text import slugify
 
-import datetime
-from .choices import CategoryChoices 
+from .choices import CategoryChoices
+
+####################
+# DEFAULTS HELPERS #
+####################
+
+
+def category_default():
+    category, _ = Category.objects.get_or_create(name="General")
+    return category.pk
+
 
 class Category(models.Model):
     name = models.CharField(unique=True, max_length=20, choices=CategoryChoices)
@@ -21,24 +30,24 @@ class Category(models.Model):
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
+
 class BlogEntry(models.Model):
-    
     title = models.CharField(primary_key=True, max_length=50, verbose_name="Blog Title")
     date_time = models.DateTimeField(default=timezone.now, verbose_name="Date & Time")
     content = models.TextField()
     category = models.ForeignKey(
-            Category,
-            on_delete=models.CASCADE,
-            related_name="blogs",
-            )
-    slug = models.SlugField(null=False, blank=True,unique=True)
-
+        Category,
+        default=category_default,
+        on_delete=models.SET_DEFAULT,
+        related_name="blogs",
+    )
+    slug = models.SlugField(null=False, blank=True, unique=True)
 
     class Meta:
         verbose_name_plural = "Blog Entries"
 
     def get_absolute_url(self):
-        return reverse('detail', args=[self.slug])
+        return reverse("detail", args=[self.slug])
 
     def save(self, *args, **kwargs):
         if not self.slug:
