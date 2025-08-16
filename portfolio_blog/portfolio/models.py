12a589eb10f028084b6fd88934c2866bea0ba0ba
froshy <robinliu81@gmail.com>
2025-08-15 20:14:18 -0400

    foreign key widget class

--- portfolio_blog/portfolio/models.py
+++ portfolio_blog/portfolio/models.py
@@ -1,27 +1,51 @@
+import datetime
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator 
 from django.urls import reverse
 from django.utils.text import slugify
-import datetime
+
+
+###################
+# Default Helpers #
+###################
+def category_default():
+    obj, _ = Category.objects.get_or_create(name="General")
+    return obj
+
 
 # Create your models here.
 def current_year():
-    return datetime.date.today().year    
+    return datetime.date.today().year
+
+
+class Category(models.Model):
+    name = models.CharField(unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class ProjectEntry(models.Model):
-
-    proj_name = models.CharField(primary_key=True, max_length=50, verbose_name="Project Name")
+    proj_name = models.CharField(
+        primary_key=True, max_length=50, verbose_name="Project Name"
+    )
+    category = models.ForeignKey(Category, default=category_default)
     description = models.TextField(verbose_name="Description")
-    start_year = models.IntegerField(null=False, verbose_name="Development Start Year",
-                                     validators=[MinValueValidator(1900), MaxValueValidator(current_year)], help_text="Enter a year between 1900 and current year")
+    start_year = models.IntegerField(
+        null=False,
+        verbose_name="Development Start Year",
+        validators=[MinValueValidator(1900), MaxValueValidator(current_year)],
+        help_text="Enter a year between 1900 and current year",
+    )
     git_repo = models.URLField(null=False, verbose_name="Github Repository")
     slug = models.SlugField(null=False, blank=True, unique=True)
-    
+
     class Meta:
         verbose_name_plural = "Project Entries"
 
     def get_absolute_url(self):
-        return reverse('detail', args=[self.slug])
+        return reverse("detail", args=[self.slug])
 
     def save(self, *args, **kwargs):
         if not self.slug:
--- portfolio_blog/portfolio/resources.py
+++ portfolio_blog/portfolio/resources.py
@@ -1,7 +1,22 @@
-from import_export import resources
+from django.core.exceptions import ObjectDoesNotExist
+from import_export import fields, resources
+from import_export.widgets import ForeignKeyWidget
+
 from .models import ProjectEntry
 
+
+class AutoForeignKeyWidget(ForeignKeyWidget):
+    def clean(self, value, row=None, **kwargs):
+        try:
+            return super().clean(value, row, **kwargs)
+        except ObjectDoesNotExist:
+            obj, _ = self.model.objects.get_or_create(**{self.field: "name"})
+            return obj
+
+
 class ProjectEntryResource(resources.ModelResource):
+    catalog = fields.Field(widget=ForeignKeyWidget)
+
     class Meta:
         model = ProjectEntry
-        import_id_fields = ['proj_name']
+        import_id_fields = ["proj_name"]
